ff898ae483cdc4d8aa99f3acd1d5570bd80227e2
Yangjialin1989 <729850713@qq.com>
2023-01-01 22:08:58 +0800

    email

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,10 @@ var router = express.Router();
 var Users = require('../models/users');
 /* GET users listing. */
 const redis = require('../utils/redis');
-
+// 引入 nodemailer
+var nodemailer = require('nodemailer');
+const Transporter = require('../utils/email');
+const GraceInfos = require("../models/graceInfos");
 router.post('/validUsername', function(req, res, next) {
     let param = {
         name:req.body.username
@@ -142,11 +145,68 @@ router.post('/register',function(req,res,next){
         console.log("存储数据成功!")
         res.json({
             code: 200,
-            msg:'注册成功！'
+            msg:'注册成功！',
+            email:req.body.email,
+            name:req.body.name
         })
     })
 
 })
+// router.get('/email',function(req,res,next){
+//     console.log('hhhh')
+//     var config = {
+//         host: 'smtp.qq.com',
+//         port: 465,
+//         auth: {
+//             user: '729850713@qq.com', //刚才注册的邮箱账号
+//             pass: 'qyvqxkvtmgnqbgaj' //邮箱的授权码，不是注册时的密码
+//         }
+//     };
+// // 创建一个SMTP客户端对象
+//     var transporter = nodemailer.createTransport(config);
+// // 发送邮件
+//     let options = {
+//         from:'729850713@qq.com',
+//         to:'1584302686@qq.com',
+//         subject:'欢迎！',
+//         html:`<div><h1>欢迎注册后台管理系统</h1></div>`
+//
+//     }
+//     transporter.sendMail(options,function(err,msg){
+//         if(err){
+//             console.log(err)
+//         }else{
+//             res.send(msg)
+//             transporter.close()
+//         }
+//     })
+// })
+router.post('/sendemail',function(req,res,next){
+    Transporter(req.body.email,req.body.name)
+})
+//获取数据
+router.post('/getadmins',function(req,res,next){
+    let param = {}
+    let param1 = {limit:req.body.limit*16}
+    console.log(param1)
+    Users.find({},{name:1,email:1,telephone:1},param1,function(err,result){
+        console.log(result)
+        if(err){
+            res.json({
+                code:104,
+                msg:'数据获取失败！',
+                data:''
+            })
+        }
+        res.json({
+            code: 200,
+            msg:'数据获取成功！',
+            data:result
+        })
+
+    })
+})
+
 
 
 module.exports = router;
--- utils/email.js
+++ utils/email.js
@@ -6,15 +6,22 @@ var config = {
   host: 'smtp.qq.com',
   port: 465,
   auth: {
-    user: '729850713.com', //刚才注册的邮箱账号
-    pass: 'cpiicovzlfcsbche' //邮箱的授权码，不是注册时的密码
+    user: '729850713@qq.com', //刚才注册的邮箱账号
+    pass: 'qyvqxkvtmgnqbgaj' //邮箱的授权码，不是注册时的密码
   }
 };
 // 创建一个SMTP客户端对象
-var transporter = nodemailer.createTransport(config);
+var Transporter = nodemailer.createTransport(config);
 // 发送邮件
-module.exports = function (mail){
-  transporter.sendMail(mail, function(error, info){
+module.exports = function (mail,msg){
+
+  options = {
+    from:'729850713@qq.com',
+    to:mail,
+    subject:'欢迎！',
+    html:`<div><h1>欢迎${msg}，注册后台管理系统！</h1></div>`
+  }
+  Transporter.sendMail(options, function(error, info){
     if(error) {
       return console.log(error);
     }
